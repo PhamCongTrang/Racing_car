9cb79701fd2710708d12074b3c567b40e0ceeb84
Vũ Huy Hoàng <vuhuyhoang.hust@gmail.com>
2022-11-09 09:56:38 +0700

    Check check

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,5 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int i;
-  // Pham Cong Trang
-  // sua tren web
-<<<<<<< HEAD
-  // Hoang is here
-=======
-  //test sang ngay 9/11 Trang
-  // sua web lan 2
-  // Hoang sua web
->>>>>>> 85d38f8622f441fe8da6c86763b0c5c7f7cd5a60
+// aaaa
 }
