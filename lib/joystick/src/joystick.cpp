1a8e0c4f38eb3ce0d49e2d0c41afe584fd289f52
Vũ Huy Hoàng <vuhuyhoang.hust@gmail.com>
2022-11-11 15:07:55 +0700

    Upate src joystick

--- lib/joystick/src/joystick.cpp
+++ lib/joystick/src/joystick.cpp
@@ -1,1 +1,1 @@
-
+#include "joystick.h"
--- lib/joystick/src/joystick.h
+++ lib/joystick/src/joystick.h
@@ -1,6 +1,28 @@
 #ifndef joystick
 #define joystick
 #include <Arduino.h>
-int joystickControl(int a, int b, int mode);
+class joystickControl
+{
+public:
+    int x;
+    int y;
+    int xmax;
+    int ymax;
+    int xmin;
+    int ymin;
+
+private:
+    int _vel;
+    int _theta;
+    joystickControl(int _x, int _y, int _xmax, int _ymax, int _xmin, int _ymin)
+    {
+        x = _x;
+        y = _y;
+        xmin = _x;
+        xmax = _xmax;
+        ymin = _ymin;
+        ymax = _ymax;
+        ymin = ymin;
+    }
+}
 #endif
-    
