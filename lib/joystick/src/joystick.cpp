e1703f048abcad09372f3dc3b3455ebd32173054
Vũ Huy Hoàng <vuhuyhoang.hust@gmail.com>
2022-11-13 09:46:01 +0700

    Edit joystick lib: change vel and phi

--- lib/joystick/src/joystick.cpp
+++ lib/joystick/src/joystick.cpp
@@ -4,11 +4,11 @@
 joystick::joystick(float _x, float _y, float _xmax, float _ymax, float _xmin, float _ymin)
 {
     this->x = _x-(_xmax+_xmin)/2;
-    this->y = y;
+    this->y = _y-(_ymax+_ymin)/2;
     this->xmax = _xmax;
     this->ymax = _ymax;
     this->xmin = _xmin;
     this->ymin = _ymin;
     this->vel =  floor(255*2/(_xmax+_xmin)*x);
-    this->phi =  180/(_ymax-_ymin)*y;
+    this->phi =  90 + 120/(_ymax-_ymin)*y;
 }
