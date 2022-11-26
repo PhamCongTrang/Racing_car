91c7e1d41a54c7ae18e2733078bc3fd624c7cce8
unknown <trang.pc200631@sis.hust.edu.vn>
2022-11-27 00:52:32 +0700

    5RC

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include "SetPID.h"
 
 // Define cac chan dieu khien dong co
-#define SER 5 // Dao cho cho ENA
+#define SER 5 
 #define ENA 6
 #define IN3 8
 #define IN4 7
@@ -27,7 +27,7 @@ int msg[3];
 
 // Define sensor
 QTRSensors qtr;
-const uint8_t SensorCount = 8;
+const uint8_t SensorCount = 5;
 uint16_t sensorValues[SensorCount];
 int valueLine[8];
 
@@ -47,7 +47,7 @@ void setup()
 
   // configure the sensors
   qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, 3,4}, SensorCount);
+  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4}, SensorCount);
   qtr.setEmitterPin(2);
 }
 
@@ -81,54 +81,31 @@ void loop()
         Serial.print(valueLine[i]);
       }
         
-      position = valueLine[0] + valueLine[1] * 2 + valueLine[2] * 4 + valueLine[3] * 8 + valueLine[4] * 16 + valueLine[5] * 32 + valueLine[6] * 64 + valueLine[7] * 128;
+      position = valueLine[0] + valueLine[1] * 2 + valueLine[2] * 4 + valueLine[3] * 8 + valueLine[4] * 16;
       //Serial.print(position);
       switch (position)
       {
-        case 0b00000000:  error = error / abs(error);  break;
-        case 0b00000001:  error = 16;  break;
-        case 0b00000010:  error = 12 + 4;  break;
-        case 0b00000100:  error = 8 + 3;  break;
-        case 0b00001000:  error = 4 + 3;  break;
-        case 0b00010000:  error = -4 - 3;  break;
-        case 0b00100000:  error = -8 - 3;  break;
-        case 0b01000000:  error = -12 - 4;  break;
-        case 0b10000000:  error = -16;  break;
+        case 0b00000:  error = error / abs(error);  break;
 
-        case 0b00000011:  error = 14 + 2;  break;
-        case 0b00000110:  error = 10 + 4;  break;
-        case 0b00001100:  error = 6 + 3;  break;
-        case 0b00011000:  error = 0;  break;
-        case 0b00110000:  error = -6 - 3;  break;
-        case 0b01100000:  error = -10 - 4;  break;
-        case 0b11000000:  error = -14 - 2;  break;
+        case 0b00001:  error = 16;  break;
+        case 0b00010:  error = 8;  break;
+        case 0b00100:  error = 0;  break;
+        case 0b01000:  error = -8;  break;
+        case 0b10000:  error = -16;  break;
 
-        case 0b00000111:  error = 12 + 4;  break;
-        case 0b00001110:  error = 8 + 3;  break;
-        case 0b00011100:  error = 4 + 2;  break;
-        case 0b00111000:  error = -4 + 2;  break;
-        case 0b01110000:  error = -8 - 3;  break;
-        case 0b11100000:  error = -12 - 4;  break;
+        case 0b00011:  error = 12;  break;
+        case 0b00110:  error = 4;  break;
+        case 0b01100:  error = -4;  break;
+        case 0b11000:  error = -12;  break;
 
-        case 0b00001111:  error = 10 + 5;  break;
-        case 0b00011110:  error = 6 + 4;  break;
-        case 0b00111100:  error = 0;  break;
-        case 0b01111000:  error = -6 - 4;  break;
-        case 0b11110000:  error = -10 - 5;  break;
+        case 0b00111:  error = 12;  break;
+        case 0b01110:  error = 4;  break;
+        case 0b11100:  error = -4;  break;
 
-        case 0b00011111:  error = 8 + 5;  break;
-        case 0b00111110:  error = 4 + 3;  break;
-        case 0b01111100:  error = -4 - 3;  break;
-        case 0b11111000:  error = -8 - 5;  break;
+        case 0b01111:  error = 12;  break;
+        case 0b11110:  error = 4;  break;
 
-        case 0b00111111:  error = 6 + 6;  break;
-        case 0b01111110:  error = 0;  break;
-        case 0b11111100:  error = -6 - 6;  break;
-
-        case 0b01111111:  error = 4 + 4;  break;
-        case 0b11111110:  error = -4 - 4;  break;
-
-        case 0b11111111:  {if (error > 0)  error = 16;
+        case 0b11111:  {if (error > 0)  error = 16;
                           else  error = -16; } break;
         
         default:  break;
@@ -155,8 +132,8 @@ void loop()
       for (uint8_t i = 0; i < SensorCount; i++)
         if (sensorValues[i] > 2000)  valueLine[i] = 1;
         else  valueLine[i] = 0;
-      sum = valueLine[0] * (-4) + valueLine[1] * (-3) + valueLine[2] * (-2) + valueLine[3] * (-1) + valueLine[4] * 1 + valueLine[5] * 2 + valueLine[6] * 3 + valueLine[7] * 4;
-      dem = valueLine[0] + valueLine[1] + valueLine[2] + valueLine[3] + valueLine[4] + valueLine[5] + valueLine[6] + valueLine[7];
+      sum = valueLine[0] * (-2) + valueLine[1] * (-1) + valueLine[2] * (0) + valueLine[3] * (1) + valueLine[4] * 2;
+      dem = valueLine[0] + valueLine[1] + valueLine[2] + valueLine[3] + valueLine[4];
       if (dem != 0)  errnow = sum / dem;
       else  errnow = errpre / abs(errpre) * 40 / Kp;
 
